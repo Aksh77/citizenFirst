99949c107f10261c60db176e80a5738d624865cd
Akshatha Nayak <akshatha.nayak@oracle.com>
2019-11-09 14:03:06 +0530

    sign up

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
+import requests
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
+@app.route('/', methods=['GET', 'POST'])
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    if request.method == "POST":
+        user = request.form['username']
+        return render_template('keywords.html', keywords = get_keywords() )
+        #return render_template('keywords.html', myFunction = get_keywords(user) )
+    return render_template('index.html')
+
+def get_keywords():
+    return ['A', 'B', 'C', 'D', 'E']
 
 if __name__ == '__main__':
     app.run()
