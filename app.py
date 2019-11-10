8f14896d69f55e4e02dbdfdbae1204a0223fe1dc
Akshatha Nayak <akshatha.nayak@oracle.com>
2019-11-10 08:43:01 +0530

    final commit

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request
 import os
 import requests
 from keywordDetail import viewKeywords
+from ministers import returnDetails
+from newsfeed import viewNewsArticles, returnRank
 import userDetail as user
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -20,7 +23,7 @@ def index():
 @app.route('/keywords', methods=['GET', 'POST'])
 def get_keywords():
     if request.method == "POST":
-        keyword = request.form['keyword']
+        keyword = request.form['keyword'].encode('ascii','ignore')
         if (keyword not in userKeywords):
             userKeywords.append(keyword)
     return render_template('keywords.html', keywords=viewKeywords(), userKeywords=userKeywords, username=username)
@@ -29,15 +32,20 @@ def get_keywords():
 def get_newsfeed():
     if request.method == "POST":
         newUser = user.User(username, userKeywords)
-    return render_template('newsfeed.html', username=username)
-# 
-# @app.route('/cause', methods=['GET', 'POST'])
-# def get_cause():
-#     return render_template('cause.html', cause=cause)
-#
-# @app.route('/minister', methods=['GET', 'POST'])
-# def get_cause():
-#     return render_template('minister.html', name=ministerName)
+        print(newUser)
+    return render_template('newsfeed.html', articles=viewNewsArticles(userKeywords), keywords=viewKeywords())
+
+@app.route('/cause', methods=['GET', 'POST'])
+def get_cause():
+    if request.method == "POST":
+        cause = [request.form['keyword']]
+    return render_template('cause.html', articles=viewNewsArticles(cause), ranking=returnRank(cause[0]))
+
+@app.route('/minister', methods=['GET', 'POST'])
+def get_minister():
+    if request.method == "POST":
+        ministerName = request.args.get('ministerName')
+    return render_template('minister.html', minister=returnDetails(ministerName))
 
 if __name__ == '__main__':
     app.run()
--- keywordDetail.py
+++ keywordDetail.py
@@ -1,6 +1,7 @@
 import csv
+
 def viewKeywords():
-    with open('backendScripting/data/causekeywords.csv', 'r') as f:
-        reader = csv.reader(f)
-        your_list = list(reader)
-    return [keyword[0] for keyword in your_list]
+	with open('backendScripting/data/causekeywords.csv', 'r') as f:
+	    reader = csv.reader(f)
+	    your_list = list(reader)
+	return [keyword[0] for keyword in your_list[1:]]
